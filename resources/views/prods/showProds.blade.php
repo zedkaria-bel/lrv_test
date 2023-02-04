23d35f0fd65a66e875153bc4a431ff85ef235124
zedkaria-bel <zaki.198@outlook.fr>
2023-02-05 00:40:10 +0100

    display-img

--- resources/views/prods/showProds.blade.php
+++ resources/views/prods/showProds.blade.php
@@ -7,6 +7,12 @@
 
 @section('content')
     @include('prods.post-header')
+    <div class="zoom-backg w-100 h-100 d-flex justify-content-center align-items-center" style="display: none !important; z-index:99">
+        <img id="img-zoom" src="" alt="zoomed-img" style="z-index:100" height="90%">
+        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="white" class="close-img m-5 fw-bolder bi bi-x-lg" viewBox="0 0 16 16">
+            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+        </svg>
+    </div>
     <div class="content">
         <div class="container px-0">
             <div class="wil-comm d-flex flex-column my-5 col-4">
@@ -32,18 +38,20 @@
                 <table id="prods-tb" class="table table-bordered display" cellspacing="0" width="100%">
                     <thead>
                         <tr>
-                        <th scope="col">Unité</th>
-                        <th scope="col">Ligne</th>
-                        <th scope="col">Brute ( jour )</th>
-                        <th scope="col">Date</th>
-                        <th scope="col">Catégorie</th>
-                        <th scope="col">User</th>
-                        <th scope="col">Wilaya</th>
+                            <th scope="col">Avatar</th>
+                            <th scope="col">Unité</th>
+                            <th scope="col">Ligne</th>
+                            <th scope="col">Brute ( jour )</th>
+                            <th scope="col">Date</th>
+                            <th scope="col">Catégorie</th>
+                            <th scope="col">User</th>
+                            <th scope="col">Wilaya</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($prods as $prod)
                             <tr>
+                                <td class="d-flex justify-content-center"> <img class="avatar rounded" height="30px" src="{{ asset('/images/'.$prod->img) }}" alt="image"> </td>
                                 <td> {{$prod->unit->code}} </td>
                                 <td>{{$prod->line}}</td>
                                 <td>{{$prod->brute_j}}</td>
@@ -71,6 +79,16 @@
     <script>
         $(document).ready( function () {
             var table = $('#prods-tb').DataTable();
+        });
+
+        $('.avatar').click(function (){
+            $('#img-zoom').attr('src', $(this).attr('src'));
+            $('.zoom-backg').show('slow');
+        });
+
+        $('.close-img').click(function (e) {
+            $('#img-zoom').attr('src', '');
+            $('.zoom-backg').hide('slow');
         });
 
         $('#from').change(function (e) {
